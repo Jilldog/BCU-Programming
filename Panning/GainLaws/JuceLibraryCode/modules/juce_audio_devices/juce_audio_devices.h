da0bd704ace2239e83824fcf9bf178d8284e2aa1
Nick Jillings <nicholas.jillings@eecs.qmul.ac.uk>
2013-10-09 20:56:51 +0100

    Added OSX Build for Panner

--- Panning/GainLaws/JuceLibraryCode/modules/juce_audio_devices/juce_audio_devices.h
+++ Panning/GainLaws/JuceLibraryCode/modules/juce_audio_devices/juce_audio_devices.h
@@ -1,5 +1,11 @@
 // This is an auto-generated file to redirect any included
 // module headers to the correct external folder.
 
-#include "C:/JUCE/modules/juce_audio_devices/juce_audio_devices.h"
+#if defined (JUCER_VS2010_78A501D)
+ #include "C:/JUCE/modules/juce_audio_devices/juce_audio_devices.h"
+#elif defined (JUCER_XCODE_MAC_F6D2F4CF)
+ #include "/juce/modules/juce_audio_devices/juce_audio_devices.h"
+#else
+ #error "This file is designed to be used in an Introjucer-generated project!"
+#endif
 
--- Panning/GainLaws/JuceLibraryCode/modules/juce_audio_utils/juce_audio_utils.h
+++ Panning/GainLaws/JuceLibraryCode/modules/juce_audio_utils/juce_audio_utils.h
@@ -1,5 +1,11 @@
 // This is an auto-generated file to redirect any included
 // module headers to the correct external folder.
 
-#include "C:/JUCE/modules/juce_audio_utils/juce_audio_utils.h"
+#if defined (JUCER_VS2010_78A501D)
+ #include "C:/JUCE/modules/juce_audio_utils/juce_audio_utils.h"
+#elif defined (JUCER_XCODE_MAC_F6D2F4CF)
+ #include "/juce/modules/juce_audio_utils/juce_audio_utils.h"
+#else
+ #error "This file is designed to be used in an Introjucer-generated project!"
+#endif
 
