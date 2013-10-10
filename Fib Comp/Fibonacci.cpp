69560ee8fa455f87f90832660f82847bccacbdec
Nick Jillings <nicholas.jillings@eecs.qmul.ac.uk>
2013-10-10 12:03:29 +0100

    Fibonacci Theory Competition

---
+++ Fib Comp/Fibonacci.cpp
@@ -0,0 +1,20 @@
+// Blank C++ File for Fibonacci Theory
+
+// This include allows you to use a function called cout which prints information to the console
+#include <iostream>
+
+// The function main is called when a program first runs.
+int main()
+{
+	// These are the four variables you will need
+	unsigned long long next;
+	unsigned long long current = 1;
+	unsigned long long previous = 0;
+	unsigned int counter; // What value should counter be!
+
+	// Devise your code to generate the correct numbers, the theorum is
+	// x[n] = x[n-1] + x[n-2]. Find the 80th Number and store it in current.
+	// It can be done in a for or a while loop!
+
+	cout << "80th: "<< current;
+}
