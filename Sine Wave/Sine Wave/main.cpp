cca92ae1da48ae6861b421bcb0531484a9f02ea4
Nick Jillings <nicholas.jillings@eecs.qmul.ac.uk>
2013-10-15 12:29:36 +0100

    Added Sine Wave Test

---
+++ Sine Wave/Sine Wave/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Sine Wave
+//
+//  Created by Nicholas Jillings on 15/10/2013.
+//
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
